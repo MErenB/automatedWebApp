d88f4dac9543b4c11a37ef99696272d2691d8446
MErenB <bltmehmeteren@gmail.com>
2021-03-14 19:00:10 +0300

    First test method. Small change in computeResults

--- app/src/main/java/automatedWebApp/App.java
+++ app/src/main/java/automatedWebApp/App.java
@@ -22,7 +22,7 @@ public class App {
                 sum += integer*doubleValue;
             }            
         }
-        return message ": "+ sum;
+        return message + sum;
     }
 
 }
--- app/src/test/java/automatedWebApp/AppTest.java
+++ app/src/test/java/automatedWebApp/AppTest.java
@@ -6,9 +6,30 @@ package automatedWebApp;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 class AppTest {
     @Test void appHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
+
+    @Test void isResultCorrect() {
+        ArrayList<Integer> arrList = new ArrayList<Integer>();
+        arrList.add(0);
+        arrList.add(2);
+        arrList.add(4);
+        arrList.add(9);
+
+        Number number = Long.valueOf(1500);
+        
+
+        assertEquals("Result is:"+Double.valueOf(13500), App.computeResults(arrList,Long.valueOf(1500),4,"Result is:"));
+        assertEquals("Result is:"+Double.valueOf(65), App.computeResults(arrList,Byte.valueOf((byte) 5),3,"Result is:"));
+
+    }
+
+
+
+
 }
