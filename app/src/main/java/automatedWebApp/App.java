2598a5a4679092f95fec51b216749ef2fe8ac7ea
MErenB <bltmehmeteren@gmail.com>
2021-03-14 18:48:27 +0300

    Adding a computeResults method

--- app/src/main/java/automatedWebApp/App.java
+++ app/src/main/java/automatedWebApp/App.java
@@ -3,6 +3,8 @@
  */
 package automatedWebApp;
 
+import java.util.ArrayList;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
@@ -11,4 +13,16 @@ public class App {
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
     }
+
+    public static String computeResults(ArrayList<Integer> arrList, Number number, int lowerBound, String message ){
+        double doubleValue = number.doubleValue();
+        double sum = 0;
+        for (Integer integer : arrList) {
+            if(integer > lowerBound ) {
+                sum += integer*doubleValue;
+            }            
+        }
+        return message ": "+ sum;
+    }
+
 }
